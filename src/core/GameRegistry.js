5bbc3036dd5f1d2e9877263d43d634104aead528
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-09-08 12:37:45 +0700

    fix: keep breaking archaeology tiles after quiz appears (#11)
    * fix: allow breaking remaining archaeology tiles
    
    * fix: load continuous archaeology breaking mode

--- src/core/GameRegistry.js
+++ src/core/GameRegistry.js
@@ -34,8 +34,8 @@ export const GAME_REGISTRY = Object.freeze({
     category: 'curiosity',
     minAge: 3,
     type: '3D',
-    loader: () => import('../games/archaeology/ProgressiveArchaeologyGame.js'),
-    exportName: 'ProgressiveArchaeologyGame',
+    loader: () => import('../games/archaeology/ContinuousBreakArchaeologyGame.js'),
+    exportName: 'ContinuousBreakArchaeologyGame',
   },
 });
 
