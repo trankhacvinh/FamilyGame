8d599038cad9ef0927ca87694ffd1c8d796aa903
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-09-08 22:12:45 +0700

    feat: load weather-enabled fruit catcher

--- src/core/GameRegistry.js
+++ src/core/GameRegistry.js
@@ -46,8 +46,8 @@ export const GAME_REGISTRY = Object.freeze({
     category: 'reflex',
     minAge: 3,
     type: '2.5D',
-    loader: () => import('../games/fruit-catch/FruitCatchVisualGame.js'),
-    exportName: 'FruitCatchVisualGame',
+    loader: () => import('../games/fruit-catch/FruitCatchWeatherGame.js'),
+    exportName: 'FruitCatchWeatherGame',
   },
 });
 
