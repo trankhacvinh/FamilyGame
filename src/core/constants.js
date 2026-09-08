64ae31da02e290820c9ff6be99f360e2389494ba
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-09-08 10:25:04 +0700

    feat: register archaeology screen state

--- src/core/constants.js
+++ src/core/constants.js
@@ -2,6 +2,7 @@ export const SCREEN = Object.freeze({
   MENU: 'MENU',
   SHAPES: 'GAME_SHAPES',
   SPACE: 'GAME_SPACE',
+  ARCHAEOLOGY: 'GAME_ARCHAEOLOGY',
 });
 
 export const LANGUAGE = Object.freeze({
