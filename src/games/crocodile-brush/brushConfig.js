8423fcde0bda5097d89bbb9b4802d7f784266380
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-09-09 15:24:54 +0700

    feat: add crocodile brush config

---
+++ src/games/crocodile-brush/brushConfig.js
@@ -0,0 +1,8 @@
+export const BRUSH_CONFIG = Object.freeze({
+  toothCountPerRow: 8,
+  brushRadius: 0.72,
+  cleanTravel: 3.6,
+  bubblePoolSize: 24,
+  hintDurationMs: 3800,
+  completionDelayMs: 350,
+});
